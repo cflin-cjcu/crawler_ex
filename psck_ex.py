4e7a5cd82fe3eafbec57f1bdb31b0d5c91cf4f0d
cflin <cflin@mail.cjcu.edu.tw>
2022-07-26 18:28:10 +0800

    first commit

---
+++ psck_ex.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+payload = {"type": 0, "sort": "add_time", "p": 5, "ps": 40}
+payloadHeader = {
+    'Host': 'www.psck.net',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+}
+url = "https://www.psck.net/api/?con=api&act=diskList&lang=null"
+r = requests.post(url, data=payload, headers=payloadHeader).json()
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(r, f, ensure_ascii=False)
